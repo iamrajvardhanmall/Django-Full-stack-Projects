2d3d40219907e8fcc46ac87fb4b9826f8a08d0b1
iamrajvardhanmall <rajvardhanmall@gmail.com>
2026-06-06 14:05:18 +0530

    hotel_search_project

---
+++ hotel_search/home/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class hotel(models.Model):
+    hotel_name = models.CharField(max_length=100)
+    hotel_price = models.IntegerField()
+    hotel_description = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.hotel_name
